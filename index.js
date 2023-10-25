a958fefecc3e638e5d0f457ff2f795e1d5f8c72b
Kelvin Gil <gilkelvin2@gmail.com>
2023-10-25 13:15:03 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your code here
+
+function submitData(data,dataTwo){
+    return fetch('http://localhost:3000/users', {
+        method:'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+          },
+        body: JSON.stringify({
+            name: `${data}`,
+            email : `${dataTwo}`,
+        })
+    })
+    .then(res => res.json())
+    .then(updatedRes => document.body.textContent=updatedRes.id)
+    .catch(message => document.body.textContent=message)
+}
